5393826a8b8514fdd36a93d4f2bb1876476130e3
Ismael Velez Recio <ismael.velez@alu.uclm.es>
2021-03-26 20:40:19 +0100

    cors2

--- app.js
+++ app.js
@@ -1,60 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-require('dotenv').config();
+var indexRouter = require("./routes/index");
+var postsRouter = require("./routes/posts");
 
-var indexRouter = require('./routes/index');
-var postsRouter = require('./routes/posts');
+var bodyParser = require("body-parser"); //nuevo
+var cors = require("cors"); //nuevo
 
-var bodyParser = require("body-parser")
-var cors = require("cors")
+require("dotenv").config();
+
+var mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
+
+mongoose
+  .connect(process.env.DB_URI, { useNewUrlParser: true })
+  .then(() => console.log("mymerndb connection ok"))
+  .catch((err) => console.error(err));
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+
+app.use(cors()); //nuevo
+app.use(bodyParser.json({ limit: "50mb" })); //nuevo
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); //nuevo
+
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/posts', postsRouter);
+app.use("/", indexRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}, {useUnifiedTopology:true})
-  .then(() => console.log("mymerndb connecion ok"))
-  .catch((err) => console.error(err));
-
-app.use(cors);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 module.exports = app;
